2c0265653c95468324b2430a58e62e4123cdf533
Alexandre Brun <thealexbrun@gmail.com>
2024-12-03 13:32:37 -0500

    Optimize day03

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -4,43 +4,35 @@ use std::time::Instant;
 
 use aoc_2024::get_input_as_string;
 
-fn multiplicate_some(input: &str) -> u32 {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let mut sum: u32 = 0;
-
-    for (_, [n1, n2]) in re.captures_iter(input).map(|r| r.extract()) {
-        sum += n1.parse::<u32>().unwrap() * n2.parse::<u32>().unwrap();
-    }
-
-    sum
-}
-
-fn multiplicate_restrictive(input: &str) -> u32 {
+fn multiplicate(input: &str) -> (u32, u32) {
     let re = Regex::new(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))").unwrap();
     let mut sum: u32 = 0;
+    let mut partial_sum: u32 = 0;
     let mut is_enabled: bool = true;
 
-    for (_, m) in re.captures_iter(input).enumerate() {
+    for m in re.captures_iter(input) {
         match m.get(0).unwrap().as_str() {
             "do()" => is_enabled = true,
             "don't()" => is_enabled = false,
             _ => {
-                if is_enabled {
-                    let n1 = m.get(1).unwrap().as_str().parse::<u32>().unwrap();
-                    let n2 = m.get(2).unwrap().as_str().parse::<u32>().unwrap();
+                let n1 = m.get(1).unwrap().as_str().parse::<u32>().unwrap();
+                let n2 = m.get(2).unwrap().as_str().parse::<u32>().unwrap();
 
-                    sum += n1 * n2;
+                let r = n1 * n2;
+                sum += r;
+
+                if is_enabled {
+                    partial_sum += r;
                 }
             }
         }
     }
 
-    sum
+    (sum, partial_sum)
 }
 
 fn solve(input: &str) -> (impl Display, impl Display) {
-    let p1 = multiplicate_some(input);
-    let p2 = multiplicate_restrictive(input);
+    let (p1, p2) = multiplicate(input);
 
     (p1, p2)
 }
@@ -61,20 +53,19 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::multiplicate_restrictive;
-    use crate::multiplicate_some;
+    use crate::multiplicate;
 
     #[test]
     fn test_p1() {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        let res = multiplicate_some(&input);
+        let (res, _) = multiplicate(&input);
         assert_eq!(161, res);
     }
 
     #[test]
     fn test_p2() {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-        let res = multiplicate_restrictive(&input);
+        let (_, res) = multiplicate(&input);
 
         assert_eq!(48, res);
     }
